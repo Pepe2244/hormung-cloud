6d89ea691f1faff594e9242afaed8aa4016f2ac2
Pepe2244 <141749578+Pepe2244@users.noreply.github.com>
2026-02-09 20:08:19 +0000

    Corrigindo erro de sintaxe no config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,17 @@
-import { defineConfig } from
-'astro/config';
-import react from 'Castrojs/react';
-import tailwind from 'Castrojs/tailwind';
-import vercel from '@astrojs/vercel/ serverless':
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+
 export default defineConfig({
-integrations: react (), tailwind () ],
-output: 'server', |/ CRÍTICO: Ativa
-• O
-modo SSR (API Dinâmica)
-adapter: vercel(), |/ CRÍTICO: Prepara o
-build para Vercel
-vite: {
-server: {
-allowedhosts: l
-// Mantém compatibilidade com
-Replit se precisar voltar
-'701d6667-32a2-4850-
-b35f-6fa9f377465e-00-1zng2lfsnunsi. janeway
-• replit. dev'
-} );
+  integrations: [react(), tailwind()],
+  output: 'server',
+  adapter: vercel(),
+  vite: {
+    server: {
+      allowedHosts: [
+        '701d6667-32a2-4850-b35f-6fa9f377465e-00-1zng2lfsnunsi.janeway.replit.dev'
+      ]
+    }
+  }
+});
